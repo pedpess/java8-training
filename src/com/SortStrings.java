bb6482da4dc0c77030414f547cd5ccd2ec3e8c44
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-03-07 18:19:43 +0100

    Add method references for functional interfaces

--- src/com/SortStrings.java
+++ src/com/SortStrings.java
@@ -1,8 +1,10 @@
 package com;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class SortStrings {
 
@@ -16,15 +18,6 @@ public class SortStrings {
 
         //Comparator<String> comparator = new ComparatorForSize();
         //Collections.sort(words, comparator);
-
-        //Default method sort, concrete method into an interface (List) now using lambda
-        words.sort((s1, s2) -> {
-            if (s1.length() < s2.length())
-                return -1;
-            if (s1.length() > s2.length())
-                return 1;
-            return 0;
-        });
 
         //Enhanced lambda
         words.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
@@ -51,5 +44,17 @@ public class SortStrings {
         words.forEach(word -> {
             System.out.println(word);
         });
+
+        //Both works in the very same way using method reference in the last version
+        words.sort(Comparator.comparing(s -> s.length()));
+        words.sort(Comparator.comparing(String::length));
+
+
+        Function<String, Integer> function = s -> s.length();
+        Comparator<String> comparator = Comparator.comparing(function);
+        words.sort(comparator);
+
+        //Another Method Reference
+        words.forEach(System.out::println);
     }
 }
